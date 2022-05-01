23420cdd24bf93f9864a40d4f78640d80215efdc
ismailidn1 <ismail@sicepat.com>
2022-05-01 10:17:21 +0700

    Update homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,10 +31,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            icon: Icon(Icons.home, color: Color(0xff006EEE)),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.safety_divider, color: Color(0xff006EEE)),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_sharp, color: Color(0xff006EEE)),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundImage: AssetImage('assets/user_1.png'),
+              radius: 15.0,
+            ),
+            label: '',
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
-            height: 800.0,
             padding: EdgeInsets.all(12.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
@@ -61,43 +84,48 @@ class _HomePageState extends State<HomePage> {
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 20),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: cardOne.length,
-                    itemBuilder: (BuildContext context, int index) => Container(
-                      width: 120,
-                      // height: 120,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            const Radius.circular(12.0),
+                SizedBox(
+                  height: 150,
+                  child: Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: cardOne.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          Container(
+                        width: 120,
+                        height: 120,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              const Radius.circular(12.0),
+                            ),
                           ),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              Image.asset(
-                                '${cardOne[index].asset}',
-                                width: 42,
-                                height: 42,
-                              ),
-                              SizedBox(height: 17),
-                              Text(
-                                cardOne[index].name,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 5),
-                              Text(
-                                '${cardOne[index].totalCourse} Courses',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    color: Colors.grey),
-                              ),
-                            ],
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Image.asset(
+                                  '${cardOne[index].asset}',
+                                  width: 42,
+                                  height: 42,
+                                ),
+                                SizedBox(height: 17),
+                                Text(
+                                  cardOne[index].name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  '${cardOne[index].totalCourse} Courses',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.grey),
+                                ),
+                              ],
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
                           ),
                         ),
                       ),
@@ -124,23 +152,140 @@ class _HomePageState extends State<HomePage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                 ),
                 SizedBox(height: 10),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: 2,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, int index) => Container(
-                      width: 180,
-                      height: 250,
-                      child: Card(
-                        child: Column(
-                          children: [
-                            Image.asset('assets/card_1.png'),
-                            SizedBox(height: 10),
-                            Text('Free'),
-                            Text('UI Design : Wireframe to Visual Design'),
-                            Text('Rating'),
-                          ],
+                SizedBox(
+                  height: 205,
+                  child: Expanded(
+                    child: ListView.builder(
+                      itemCount: 2,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, int index) => ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
                         ),
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 9.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(30.0),
+                            ),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(.0, 1.0), //(x,y)
+                                blurRadius: 5.0,
+                              ),
+                            ],
+                          ),
+                          width: 180,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/card_${index + 1}.png',
+                                fit: BoxFit.cover,
+                                width: 180,
+                                height: 100,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 10.0, top: 10.0),
+                                child: Text(
+                                  'Free',
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 10.0, top: 5.0),
+                                child: Text(
+                                  'UI Design : Wireframe to Visual Design',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.0,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 10.0, top: 7.0),
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.star),
+                                    Icon(Icons.star),
+                                    Icon(Icons.star),
+                                    Icon(Icons.star),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "Article",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Show All",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800]),
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: 3,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, int index) => Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                      ),
+                      margin: EdgeInsets.all(10.0),
+                      height: 80.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20.0),
+                              bottomLeft: Radius.circular(20.0),
+                            ),
+                            child: Image.asset(
+                              'assets/card_${index + 1}.png',
+                              fit: BoxFit.cover,
+                              width: 86,
+                              height: 80,
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('123'),
+                              Text('123'),
+                              Text('123'),
+                            ],
+                          ),
+                          Text('asd'),
+                        ],
                       ),
                     ),
                   ),
